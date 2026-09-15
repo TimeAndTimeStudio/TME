d89ebbc19b8ad71679f68e1f63d400a92513f78a
TimeAndTimeStudio <nok.craft.studio@gmail.com>
2026-09-15 22:18:55 +0700

    fix: add debug logging to draw

---
+++ examples/jump-game/game.js
@@ -0,0 +1,25 @@
+let player = { x: 100, y: 400, vy: 0 };
+let JUMP_FORCE = 300;
+let GRAVITY = 800;
+let GROUND = 400;
+function update(dt) {
+  if (input.keyboard.is_down("space")) {
+    player.vy = (0 - JUMP_FORCE);
+  }
+  player.vy = (player.vy + (GRAVITY * dt));
+  player.y = (player.y + (player.vy * dt));
+  if ((player.y > GROUND)) {
+    player.y = GROUND;
+    player.vy = 0;
+  }
+}
+function draw() {
+  console.log('draw() called');
+  console.log('rect function:', typeof rect);
+  rect(0, GROUND, 800, 50, "brown");
+  rect(player.x, player.y, 50, 50, "blue");
+}
+window.update = update;
+window.draw = draw;
+fps(60);
+start("startBtn");
