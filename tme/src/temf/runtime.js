f00685aa9086ca4557d492426e7f27b088d9e03e
TimeAndTimeStudio <nok.craft.studio@gmail.com>
2026-09-15 21:28:57 +0700

    fix: support global update() and draw() functions

--- tme/src/temf/runtime.js
+++ tme/src/temf/runtime.js
@@ -1441,6 +1441,11 @@ function start(game, fps) {
   }
 
   TEMF._started = true;
+  if (!game) {
+    game = {};
+    if (typeof update === 'function') game.update = update;
+    if (typeof draw === 'function') game.draw = draw;
+  }
   TEMF._game = game;
   TEMF._fps = fps || 60;
   TEMF._step = 1 / TEMF._fps;
