69bb81c95780da6ca10e3c608a9c220f3236d620
TimeAndTimeStudio <nok.craft.studio@gmail.com>
2026-09-15 22:33:46 +0700

    fix: add debug logging for render pass

--- tme/src/temf/runtime.js
+++ tme/src/temf/runtime.js
@@ -1344,6 +1344,7 @@ function _draw() {
   };
 
   const renderPass = commandEncoder.beginRenderPass(renderPassDescriptor);
+  console.log('RenderPass started');
 
   const currentRects = [];
 
@@ -1371,7 +1372,9 @@ function _draw() {
   _drawImages(renderPass);
 
   renderPass.end();
+  console.log('RenderPass ended, submitting commands...');
   device.queue.submit([commandEncoder.finish()]);
+  console.log('Commands submitted');
 
   TEMF._drawList.length = 0;
 }
