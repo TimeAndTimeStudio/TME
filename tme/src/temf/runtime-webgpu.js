550248149d3ef5d2cbd33271187eccc7c5aec11b
Time And Time Studio <nok.craft.studio@gmail.com>
2026-09-17 21:12:37 +0700

    Update runtime-webgpu.js

--- tme/src/temf/runtime-webgpu.js
+++ tme/src/temf/runtime-webgpu.js
@@ -1364,59 +1364,72 @@ function _isAudio(path) {
   return _AUDIO_EXTS.includes(ext);
 }
 
-function _preloadOne(path) {
-  if (_isAudio(path)) {
-    return _loadAudioBuffer(path).then((buffer) => {
-      return !!buffer;
-    }).catch(() => false);
-  }
-  return _loadImage(path).then((img) => !!img).catch(() => false);
-}
-
 function preload(resources) {
-  if (!resources) {
-    TEMF._preloadDone = true;
-    return Promise.resolve([]);
-  }
-
   let paths = [];
 
-  if (typeof resources === 'string') {
-    paths = [resources];
-  } else if (Array.isArray(resources)) {
-    for (const item of resources) {
-      if (typeof item === 'string') {
-        paths.push(item);
-      } else if (item && typeof item === 'object') {
-        if (item.images) paths = paths.concat(item.images);
-        if (item.bgm) paths = paths.concat(item.bgm);
-        if (item.sfx) paths = paths.concat(item.sfx);
-        if (item.path) paths.push(item.path);
+  if (resources) {
+    if (typeof resources === 'string') {
+      paths = [resources];
+    } else if (Array.isArray(resources)) {
+      for (const item of resources) {
+        if (typeof item === 'string') {
+          paths.push(item);
+        } else if (item && typeof item === 'object') {
+          if (item.images) paths = paths.concat(item.images);
+          if (item.bgm) paths = paths.concat(item.bgm);
+          if (item.sfx) paths = paths.concat(item.sfx);
+          if (item.path) paths.push(item.path);
+        }
       }
     }
   }
 
-  const total = paths.length;
-  if (total === 0) {
+  TEMF._preloadFailed = [];
+
+  if (paths.length === 0) {
     TEMF._preloadDone = true;
-    return Promise.resolve([]);
-  }
+  } else {
+    TEMF._preloadDone = false;
 
-  TEMF._preloadDone = false;
-  let failed = [];
+    Promise.all(
+      paths.map(async (path) => {
+        try {
+          if (_isAudio(path)) {
+            const buffer = await _loadAudioBuffer(path);
 
-  return Promise.all(paths.map((path) => {
-    return _preloadOne(path).then((success) => {
-      if (!success) {
-        failed.push(path);
-      }
-      return success;
+            if (!buffer) {
+              TEMF._preloadFailed.push(path);
+            }
+          } else {
+            const img = await _loadImage(path);
+
+            if (img && TEMF._device) {
+              const texture = await _createTexture(TEMF._device, img);
+
+              if (texture) {
+                TEMF._textureCache.set(path, {
+                  status: 'loaded',
+                  texture: texture,
+                  width: img.width,
+                  height: img.height,
+                  lastUsed: performance.now()
+                });
+              } else {
+                TEMF._preloadFailed.push(path);
+              }
+            } else {
+              TEMF._preloadFailed.push(path);
+            }
+          }
+        } catch (err) {
+          TEMF._preloadFailed.push(path);
+          console.error('Preload failed:', path, err);
+        }
+      })
+    ).then(() => {
+      TEMF._preloadDone = true;
     });
-  })).then((results) => {
-    TEMF._preloadDone = true;
-    TEMF._preloadFailed = failed;
-    return results;
-  });
+  }
 }
 
 function checkpreload() {
