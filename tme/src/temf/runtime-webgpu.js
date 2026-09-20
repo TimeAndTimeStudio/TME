7ff236704af97281daaf525617541f34207ec89a
TimeAndTimeStudio <nok.craft.studio@gmail.com>
2026-09-20 11:19:37 +0700

    Add GPL v3 license header to all source files

--- tme/src/temf/runtime-webgpu.js
+++ tme/src/temf/runtime-webgpu.js
@@ -1,5 +1,22 @@
 /**
  * TEMF — Time Engine Mini Fast Runtime
+ * 
+ * Copyright (C) 2026 Time And Time Studio
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * 
+ * Date: 20/09/2026
  *
  * Rendering: rect + image draw calls, transform (rotation, scale) and alpha
  * for both. Textures are cached by path (LRU-evicted) and uploaded via
